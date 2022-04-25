60a79097cf659429b1e9bbf96f9e77f32205551e
topo978 <58426294+topo978@users.noreply.github.com>
2022-04-26 01:17:02 +0200

    Add files via upload

---
+++ lib/theme/colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+const Color primary = Color.fromARGB(255, 5, 122, 89);
+const Color secondary = Color(0xFF0A3368);
+const Color primaryLight = Color.fromARGB(255, 22, 206, 151);
+const Color white = Color(0xFFFFFFFF);
+const Color black = Color(0xFF101111);
+const Color grey = Color(0xFF3a3a3a);
+const Color thirdColor = Color(0xFFC71C1C);
+const Color fourthColor = Color(0xFF910391);
